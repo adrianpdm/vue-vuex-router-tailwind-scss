87032768e476a146e59054320ed42b13b498fd3e
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-04-28 16:53:03 +0700

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.js",
+    // etc.
+  ],
+  css: ["./src/assets/stylesheet/main.scss"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelist: ["html", "body", "button", "input"],
+  whitelistPatterns: [],
+})
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+}
